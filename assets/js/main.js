51de13a5d4701eaf840b368ceba222aaae4c230e
alvin-tosh <alvingitonga14@gmail.com>
2022-06-05 14:58:19 +0300

    sf

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Folio - v4.7.0
-* Template URL: https://bootstrapmade.com/folio-bootstrap-portfolio-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
